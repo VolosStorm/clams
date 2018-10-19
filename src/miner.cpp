717cef31d459eb3f290a5607dfec47bdca5f4091
Chris Moore <dooglus@gmail.com>
2018-10-19 12:20:37 -0700

    Include transactions in the blocks we stake.

--- src/miner.cpp
+++ src/miner.cpp
@@ -191,6 +191,8 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
     // transaction (which in most cases can be a no-op).
     fIncludeWitness = IsWitnessEnabled(pindexPrev, chainparams.GetConsensus());
 
+    addPackageTxs();
+
     nLastBlockTx = nBlockTx;
     nLastBlockSize = nBlockSize;
     nLastBlockWeight = nBlockWeight;
