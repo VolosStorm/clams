b3d8c19c118c07cc11a95dab5c47ea42e949fe5e
Chris Moore <dooglus@gmail.com>
2018-10-28 08:03:57 -0700

    Use .clam folder instead of .bitcoin.

--- src/util.cpp
+++ src/util.cpp
@@ -504,13 +504,13 @@ void PrintExceptionContinue(const std::exception* pex, const char* pszThread)
 boost::filesystem::path GetDefaultDataDir()
 {
     namespace fs = boost::filesystem;
-    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Bitcoin
-    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Bitcoin
-    // Mac: ~/Library/Application Support/Bitcoin
-    // Unix: ~/.bitcoin
+    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Clam
+    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Clam
+    // Mac: ~/Library/Application Support/Clam
+    // Unix: ~/.clam
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Clam";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -520,10 +520,10 @@ boost::filesystem::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/Clam";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / ".clam";
 #endif
 #endif
 }
