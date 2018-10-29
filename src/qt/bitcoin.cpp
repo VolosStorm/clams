29ad478685d28c70e009f4ae312b912d288a41a1
Chris Moore <dooglus@gmail.com>
2018-10-28 17:52:38 -0700

    bitcoin -> clam

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -546,7 +546,7 @@ int main(int argc, char *argv[])
 #endif
 
     Q_INIT_RESOURCE(bitcoin);
-    Q_INIT_RESOURCE(bitcoin_locale);
+    Q_INIT_RESOURCE(clam_locale);
 
     BitcoinApplication app(argc, argv);
 #if QT_VERSION > 0x050100
