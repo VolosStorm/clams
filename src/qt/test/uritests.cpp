340815a65d7268eb2cdb3ff54d2868f2a1124979
deb <deb@debian.local>
2019-01-06 17:07:30 -0800

    Remove 'Wikipedia' from example address description.

--- src/qt/test/uritests.cpp
+++ src/qt/test/uritests.cpp
@@ -22,10 +22,10 @@ void URITests::uriTests()
     QVERIFY(rv.label == QString());
     QVERIFY(rv.amount == 0);
 
-    uri.setUrl(QString("clam:xGv7mBkMcvCN1DF2xkXy1beaBqhiYi4Mdo?label=Wikipedia Example Address"));
+    uri.setUrl(QString("clam:xGv7mBkMcvCN1DF2xkXy1beaBqhiYi4Mdo?label=Example Address"));
     QVERIFY(GUIUtil::parseBitcoinURI(uri, &rv));
     QVERIFY(rv.address == QString("xGv7mBkMcvCN1DF2xkXy1beaBqhiYi4Mdo"));
-    QVERIFY(rv.label == QString("Wikipedia Example Address"));
+    QVERIFY(rv.label == QString("Example Address"));
     QVERIFY(rv.amount == 0);
 
     uri.setUrl(QString("clam:xGv7mBkMcvCN1DF2xkXy1beaBqhiYi4Mdo?amount=0.001"));
@@ -40,27 +40,27 @@ void URITests::uriTests()
     QVERIFY(rv.label == QString());
     QVERIFY(rv.amount == 100100000);
 
-    uri.setUrl(QString("clam:xGv7mBkMcvCN1DF2xkXy1beaBqhiYi4Mdo?amount=100&label=Wikipedia Example"));
+    uri.setUrl(QString("clam:xGv7mBkMcvCN1DF2xkXy1beaBqhiYi4Mdo?amount=100&label=Example"));
     QVERIFY(GUIUtil::parseBitcoinURI(uri, &rv));
     QVERIFY(rv.address == QString("xGv7mBkMcvCN1DF2xkXy1beaBqhiYi4Mdo"));
     QVERIFY(rv.amount == 10000000000LL);
-    QVERIFY(rv.label == QString("Wikipedia Example"));
+    QVERIFY(rv.label == QString("Example"));
 
-    uri.setUrl(QString("clam:xGv7mBkMcvCN1DF2xkXy1beaBqhiYi4Mdo?message=Wikipedia Example Address"));
+    uri.setUrl(QString("clam:xGv7mBkMcvCN1DF2xkXy1beaBqhiYi4Mdo?message=Example Address"));
     QVERIFY(GUIUtil::parseBitcoinURI(uri, &rv));
     QVERIFY(rv.address == QString("xGv7mBkMcvCN1DF2xkXy1beaBqhiYi4Mdo"));
     QVERIFY(rv.label == QString());
 
-    QVERIFY(GUIUtil::parseBitcoinURI("clam://xGv7mBkMcvCN1DF2xkXy1beaBqhiYi4Mdo?message=Wikipedia Example Address", &rv));
+    QVERIFY(GUIUtil::parseBitcoinURI("clam://xGv7mBkMcvCN1DF2xkXy1beaBqhiYi4Mdo?message=Example Address", &rv));
     QVERIFY(rv.address == QString("xGv7mBkMcvCN1DF2xkXy1beaBqhiYi4Mdo"));
     QVERIFY(rv.label == QString());
 
-    uri.setUrl(QString("clam:xGv7mBkMcvCN1DF2xkXy1beaBqhiYi4Mdo?req-message=Wikipedia Example Address"));
+    uri.setUrl(QString("clam:xGv7mBkMcvCN1DF2xkXy1beaBqhiYi4Mdo?req-message=Example Address"));
     QVERIFY(GUIUtil::parseBitcoinURI(uri, &rv));
 
-    uri.setUrl(QString("clam:xGv7mBkMcvCN1DF2xkXy1beaBqhiYi4Mdo?amount=1,000&label=Wikipedia Example"));
+    uri.setUrl(QString("clam:xGv7mBkMcvCN1DF2xkXy1beaBqhiYi4Mdo?amount=1,000&label=Example"));
     QVERIFY(!GUIUtil::parseBitcoinURI(uri, &rv));
 
-    uri.setUrl(QString("clam:xGv7mBkMcvCN1DF2xkXy1beaBqhiYi4Mdo?amount=1,000.0&label=Wikipedia Example"));
+    uri.setUrl(QString("clam:xGv7mBkMcvCN1DF2xkXy1beaBqhiYi4Mdo?amount=1,000.0&label=Example"));
     QVERIFY(!GUIUtil::parseBitcoinURI(uri, &rv));
 }
